4089cd0e972755b9ac775e3f203c2c0f020e0827
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-05-09 23:47:15 +0600

    serialization in services

--- service/urls.py
+++ service/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .serializers import ContractUsSerializer
 from . import views
 
 router = DefaultRouter()
-router.register('', views.ContractUsViewSet)
+router.register('', views.ServiceViewSet)
 
-# The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- smart_care/urls.py
+++ smart_care/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contract_us.urls')),
+    path('contract_us/', include('contract_us.urls')),
+    path('services/', include('service.urls')),
 ]
 
 if settings.DEBUG:
