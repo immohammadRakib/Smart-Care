90736905cf6852d28055250292376ca923c9c799
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-05-09 17:45:54 +0600

    doctor_model

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Doctor, Designation, AvailableTime, Specialization
 
 # Register your models here.
+class SpeacializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',),}
+
+class DesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',),}
+
+admin.site.register(Doctor)
+admin.site.register(Designation)
+admin.site.register(AvailableTime)
+admin.site.register(Specialization)
--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Specialization(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Designation(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class AvailableTime(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Doctor(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    image = models.ImageField(upload_to='doctor/images/')
+    designation = models.CharField(Designation)
+    speacialization = models.ManyToManyField(Specialization)
+    available_time = models.ManyToManyField(AvailableTime)
+    fee = models.IntegerField()
+    meet_link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
